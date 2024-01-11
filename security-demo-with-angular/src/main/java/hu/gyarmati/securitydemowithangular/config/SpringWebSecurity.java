8d58af8e207beefecec97a8a0771da759c9002af
gyarmatiadam <gyari12@gmail.com>
2024-01-11 17:46:35 +0100

    try again

--- security-demo-with-angular/src/main/java/hu/gyarmati/securitydemowithangular/config/SpringWebSecurity.java
+++ security-demo-with-angular/src/main/java/hu/gyarmati/securitydemowithangular/config/SpringWebSecurity.java
@@ -29,7 +29,7 @@ public class SpringWebSecurity implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost", "http://127.0.0.1")
+                .allowedOrigins("172.18.0.3", "http://172.18.0.3")
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
                 .allowedHeaders("*");
     }
